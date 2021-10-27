e1848a7c27f57d36ec86f6eac7e34efb40e6ecb4
holmesrm88 <holmesrm88@gmail.com>
2021-10-27 19:11:11 -0400

    first commit

---
+++ src/main/java/com/example/msscholmesbreweryclient/MsscHolmesBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.msscholmesbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscHolmesBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscHolmesBreweryClientApplication.class, args);
+	}
+
+}
