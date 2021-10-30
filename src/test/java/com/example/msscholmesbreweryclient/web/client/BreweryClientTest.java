f25a6a6b45205f5c0fafecb0bd82029e26ecbca6
holmesrm88 <holmesrm88@gmail.com>
2021-10-29 22:12:11 -0400

    commit

---
+++ src/test/java/com/example/msscholmesbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,40 @@
+package com.example.msscholmesbreweryclient.web.client;
+
+import com.example.msscholmesbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient breweryClient;
+
+    @Test
+    void getBeerById() {
+        BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
+
+        assertNotNull(beerDto);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        URI uri = breweryClient.saveNewBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdateBeer(){
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+        breweryClient.updateBeer(UUID.randomUUID(), beerDto);
+    }
+}
